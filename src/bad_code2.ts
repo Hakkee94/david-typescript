febf7d185163bf5eb15f80f6f6ebf48a94291828
Hakkee94 <agasyandavid94@gmail.com>
2022-09-19 09:26:00 +0300

    commit

--- src/bad_code2.ts
+++ src/bad_code2.ts
@@ -1,18 +1,18 @@
-function deliveryMethod() :string {
+function deliveryMethod(): string {
   // TODO
   return 'overnight'
 }
 
-function shipWeight() {
-  const el = document.getElementById('weight')
+function shipWeight(): number {
+  const el: HTMLElement = document.getElementById('weight')
   if (!el) {
     return 0
   }
   return parseInt(el.innerHTML)
 }
 
-function sendUpdates(emailAddr) {
-  function sendEmail(addr) {
+function sendUpdates(emailAddr: string | string[]): void {
+  function sendEmail(addr: string): void {
     console.log(`Shipping to ${addr} via ${deliveryMethod() || 'standard'} delivery`)
     if (shipWeight() > 100) {
       console.log('WARNING: Oversize package')
